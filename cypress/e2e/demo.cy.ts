5ec8170ce6e00151ea7437a483d8b10c6aece7f7
Maksym Makar <maksym.makar@userway.org>
2024-07-16 16:24:59 +0200

    init main overall with 3 pages

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,5 +1,8 @@
 it('should have the right text in the headline', () => {
-  cy.visit('/');
-
-  cy.get('.slider-area').contains('property Searching Just Got So Easy');
+  cy.visit('/property-1.html');
+  cy.userwayAnalysis();
+  cy.visit('/property-2.html');
+  cy.userwayAnalysis();
+  cy.visit('/property-3.html');
+  cy.userwayAnalysis();
 });
