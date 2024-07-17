dcbc45e23541f3d74718a7bd0bd25c5e934c9dac
Alex <alex@Noutbuk-Alex.local>
2024-07-17 16:50:47 +0300

    verbose true

--- userway.config.ts
+++ userway.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from '@userway/cicd-cli' 
-    export default { 
-      organization: "maksym-makar-27-userway-org-lsiec",
-      project: "test-website-5-real-estate-gitlab",
-      reportPaths: ["./uw-a11y-reports"],
-      token: process.env.USERWAY_TOKEN
-    } satisfies Config;
+import type { Config } from "@userway/cicd-cli";
+export default {
+  organization: "maksym-makar-27-userway-org-lsiec",
+  project: "test-website-5-real-estate-gitlab",
+  reportPaths: ["./uw-a11y-reports"],
+  token: process.env.USERWAY_TOKEN,
+} satisfies Config;
