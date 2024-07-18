fd951f02dadfb3a94e77a05c6329a80c0c7f5ace
Maksym Makar <maksym.makar@userway.org>
2024-07-18 12:17:23 +0200

    server in config

--- userway.config.ts
+++ userway.config.ts
@@ -3,5 +3,6 @@ import type { Config } from '@userway/cicd-cli'
       organization: "maksym-makar-27-userway-org-lsiec",
       project: "test-website-5-real-estate-gitlab",
       reportPaths: ["./uw-a11y-reports"],
-      token: process.env.USERWAY_TOKEN
+      token: process.env.USERWAY_TOKEN,
+      server: "https://api.qa.userway.dev"
     } satisfies Config;
