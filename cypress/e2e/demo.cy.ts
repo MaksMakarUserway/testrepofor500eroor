1511f72ac9a66a48308f8683d1a0db0267937145
Maksym Makar <maksym.makar@userway.org>
2024-07-24 17:25:41 +0200

    change branch1

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -5,4 +5,8 @@ it('should have the right text in the headline', () => {
   cy.userwayAnalysis();
   cy.visit('/property-3.html');
   cy.userwayAnalysis();
+  cy.visit('/properties-2.html');
+  cy.userwayAnalysis();
+  cy.visit('/properties-3.html');
+  cy.userwayAnalysis();
 });
