fb761b51c747cb15302f0167281e6ef7a6889c9f
Maksym Makar <maksym.makar@userway.org>
2024-07-24 16:42:06 +0200

    scan main with 3 pages

--- userway.config.ts
+++ userway.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@userway/cicd-cli' 
     export default { 
-      organization: "maksym-makar-27-userway-org-lsiec",
-      project: "test-website-5-real-estate-gitlab",
+      organization: "maksym-makar-31-userway-org-bpboh",
+      project: "test-website-5-real-estate-gitlab-testing1",
       reportPaths: ["./uw-a11y-reports"],
       token: process.env.USERWAY_TOKEN,
       server: "https://api.qa.userway.dev"
