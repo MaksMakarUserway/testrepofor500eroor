a59c04b7bca37442750521b5c8be354153b486c7
Maksym Makar <maksym.makar@userway.org>
2024-07-25 14:25:13 +0200

    retry branch2

--- userway.config.ts
+++ userway.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@userway/cicd-cli' 
     export default { 
       organization: "maksym-makar-31-userway-org-bpboh",
-      project: "test-website-5-real-estate-gitlab-testing1",
+      project: "test-website-5-real-estate-gitlab-testing2",
       reportPaths: ["./uw-a11y-reports"],
       token: process.env.USERWAY_TOKEN,
       server: "https://api.qa.userway.dev"
